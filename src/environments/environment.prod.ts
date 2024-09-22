c2d6b7faa5a1c532676000807a4fbe2f0500b354
choudharymahipal <mschoudhary81@gmail.com>
2024-09-22 20:27:57 +0530

    test github actions

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true
   //npm install -g angular-cli-ghpages
-  //ng build --base-href "https://choudharymahipal.github.io/whatsappclone/"
+  //ng build --base-href="https://choudharymahipal.github.io/whatsappclone/"
   //ngh --dir dist/whatsappclone
+
+  //npx angular-cli-ghpages --dir=dist/whatsappclone --no-silent
 };
