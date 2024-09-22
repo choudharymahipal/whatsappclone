4ae5771c0e4649694ec8679bcce73ff91a9935a6
choudharymahipal <mschoudhary81@gmail.com>
2024-09-22 19:59:20 +0530

    setup gitpages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
   production: true
+  //npm install -g angular-cli-ghpages
+  //ng build --base-href "https://choudharymahipal.github.io/whatsappclone/"
+  //ngh --dir dist/whatsappclone
 };
